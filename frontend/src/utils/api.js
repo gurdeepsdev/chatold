907d5dfc199f9844e91b9b31e7027591a6a5b6a3
gurdeepsdev <gurdeep@clickorbits.com>
2026-03-25 17:15:39 +0530

    also

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const api = axios.create({
-  baseURL: `${API_BASE}`,
+  baseURL: `${API_BASE}/api`,
   withCredentials: true,
 });
 
